ccb6b75eeb7e15b8ade4c252a06e6634adecf72d
Daniel Kondov <danielkondov91@gmail.com>
2018-06-05 23:19:07 +0300

    Details Page

--- 08. HotelSystem/client/hotel-system/src/components/hotels/HotelDetailsPage.js
+++ 08. HotelSystem/client/hotel-system/src/components/hotels/HotelDetailsPage.js
@@ -3,42 +3,48 @@ import hotelAction from '../../actions/HotelActions'
 import hotelStore from '../../stores/HotelStore'
 
 class HotelDetailsPage extends Component {
-    constructor (props){
-        super(props)
+  constructor (props) {
+    super(props)
 
-        let params = this.props.match.params
-        let id = params.id
+    let params = this.props.match.params
+    let id = params.id
 
-        this.state = {
-            id: id,
-            hotel: {}
-        }
-        
-        this.handleHotelDetailsFetched = this.handleHotelDetailsFetched.bind(this)
-        hotelStore.on(hotelStore.eventTypes.HOTEL_GOT,
-          this.handleHotelDetailsFetched)
+    this.state = {
+      id: id,
+      hotel: {}
     }
-    componentDidMount () {
-        //hotelAction.getById(this.state.id)
-    }
-    componentWillUnmount () {
-        hotelStore.removeListener(
-            hotelStore.eventTypes.HOTEL_GOT,
-            this.handleHotelDetailsFetched
-        )
-    }
-    handleHotelDetailsFetched (data) {
-        this.setState({
-            hotel: data
-        })
-    }
-    render() {
-        return (
-            <div>
-                <h1>Hotel Details {this.state.id}</h1>
-            </div>
-        )
-    }
+
+    this.handleHotelDetailsFetched = this.handleHotelDetailsFetched.bind(this)
+    hotelStore.on(hotelStore.eventTypes.HOTEL_GOT,
+      this.handleHotelDetailsFetched)
+  }
+  componentDidMount () {
+    hotelAction.getById(this.state.id)
+  }
+  componentWillUnmount () {
+    hotelStore.removeListener(
+      hotelStore.eventTypes.HOTEL_GOT,
+      this.handleHotelDetailsFetched
+    )
+  }
+  handleHotelDetailsFetched (data) {
+    this.setState({
+      hotel: data
+    })
+  }
+  render () {
+    const hotel = this.state.hotel
+    return (
+      <div className='hotel-details'>
+        <h1>Hotel Details</h1>
+        <h2>{hotel.name} - {hotel.location}</h2>
+        <h2>{hotel.numberOfRooms} rooms - {hotel.parkingSlots} available parking slots</h2>
+        <div>
+          <img src={hotel.image} alt={hotel.description} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default HotelDetailsPage
